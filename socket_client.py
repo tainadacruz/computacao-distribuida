f1f2858d1cde79357ec869d41cdc1085a15216ae
Covalsqy <gabimbergmann@yahoo.com.br>
2024-05-13 17:28:59 -0300

    Tópicos

--- socket_client.py
+++ socket_client.py
@@ -36,12 +36,21 @@ class Client:
                 break
         self.name = name
         self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, f"@{self.name}@")
+        print(f"Quais seus interesses?")
+        while True:
+            interesse = input("->")
+            self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, f"@{interesse}@")
+            accept = input("Algo mais? (Y/N)")
+            if accept == "N" or accept == "n":
+                break
+            print("Digite mais um interesse")
 
     def run(self):
         estado_enviar_msg = False
         print("Começar Loop")
-        print("Para enviar uma mensagem para outro usuário digite @username_alvo")
+        print("Para enviar uma mensagem para outro usuário digite @username_alvo ou @tópico_alvo")
         print("Para ver novas mensagens digite refresh")
+        print("Para se inscrever em um tópico digite !tópico")
         print("Para desconectar digite logoff")
         user_alvo = None
         while True:
@@ -61,10 +70,13 @@ class Client:
                         try:
                             message_received = self.socket_sub.recv_string(flags=zmq.NOBLOCK)
                             correct = message_received.split(" ")
-                            del correct[0]
+                            if correct[0] == f"@{self.name}@":
+                                del correct[0]
+                            else:
+                                correct[0] = f"Tópico: {correct[0][1:(len(correct[0]) - 1)]}\n{correct[1]}"
+                                del correct[1]
                             print(correct)
                             to_print = " ".join(correct)
-                            print(to_print)
                             print(f"{Color.RED}{to_print}{Color.RESET}")
                             print("\n")
                         except:
@@ -73,6 +85,9 @@ class Client:
                 elif user_input[0] == "@": #OPÇÃO MANDAR MENSAGEM PRA USUÁRIO ALVO
                     user_alvo = user_input[1:]
                     estado_enviar_msg = True
+                elif user_input[0] == "!": #OPÇÃO INSCREVER EM UM TÓPICO
+                    topic = user_input[1:]
+                    self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, f"@{topic}@")
                 else:
                     print("Comando não reconhecido")
 
