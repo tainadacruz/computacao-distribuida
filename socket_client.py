f6d676b5abb22abc83b53a18237797eacd57d576
Covalsqy <gabimbergmann@yahoo.com.br>
2024-05-12 22:17:08 -0300

    Commit pra deixar vcs verem o q eu to fazendo

--- socket_client.py
+++ socket_client.py
@@ -1,7 +1,4 @@
 import zmq
-import os
-import select
-import sys
 
 class Color:
     RESET = '\033[0m'
@@ -17,158 +14,68 @@ class Color:
 
 class Client:
     def __init__(self):
+        #Inscreve no socket publisher do servidor
         self.context_sub = zmq.Context()
         self.socket_sub = self.context_sub.socket(zmq.SUB)
         self.socket_sub.connect("tcp://localhost:5556")
-        self.context_resp = zmq.Context()
-        self.socket_resp = self.context_resp.socket(zmq.REQ)
-        self.socket_resp.connect("tcp://localhost:5557")  # Assuming server is listening on port 5557
-        #socket_resp.send_string(f"Received registration confirmation for client {client_id}")
+
+        #Conecta ao socket pull do servidor
         self.context_push = zmq.Context()
         self.socket_push = self.context_push.socket(zmq.PUSH)
-        #ele faz a conexão pra enviar a mensagem conforme definido o destinatário então da pra deixar sem conexão por enquanto
-        self.context_pull = zmq.Context()
-        self.socket_pull = self.context_pull.socket(zmq.PULL)
-        #self.socket_pull.setsockopt(zmq.RCVTIMEO, 10)
+        self.socket_push.connect("tcp://localhost:5557")
 
-        ######SÓ PRA TESTAR EM CONEXÃO LOCAL SEM VM######
-        try:
-            self.pull_address = "tcp://localhost:5558"
-            self.socket_pull.bind(self.pull_address)
-        except zmq.error.ZMQError as e:
-            self.pull_address = "tcp://localhost:5559"
-            self.socket_pull.bind(self.pull_address)
-        #################################################
-
+        #Executa
         self.register()
-        self.contatos = {} #sockets de usuários amigos online
-        topic = "new_users" 
-        self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, topic) #só se registra no tópico de novos usuários depois de se registrar para não ser notificado sobre a própria inscrição
         self.run()
 
-    def register(self, flag=0, escolhas_topico = ""):
-        if flag == 0: #caso "username inv" ele pode só tentar se registrar de novo sem selecionar "login ou novo usuário?" novamente
-            login = input("Login (1)\nNovo usuário (2)\n ->")
-        else:
-            login = 2
-
-        name = input("Login: ")
-        password = input("Senha: ")
+    def register(self):
+        while True:
+            name = input("Insira seu email\n->")
+            confirm = input(f"O email {name} está correto?(Y/N)\n->")
+            if confirm == "Y":
+                break
         self.name = name
-        if login == "1":
-            message_regist = f"login {name} {password} {self.pull_address}" #mensagem pro servidor com login e senha para ver se já é um usuário registrado formato [operação] [dado dado dado... dado]. trata mensagem no server
-            self.socket_resp.send_string(message_regist) #manda pro servidor
-            resposta = self.socket_resp.recv_string() #analisa resposta
-            if resposta == "liberado": #se já existe e senha correta
-                print(f"Bem vindo de volta, {name}")
-
-            elif resposta == "err_senha":
-                print("Senha errada. Registre um novo usuário ou tente outra senha")
-                self.register()
-        else: #caso contrário
-            if escolhas_topico == "":
-                interesses = input("Digite tópicos de interesse (1 palavra por tópico): ")
-                interesses = interesses.split(" ")
-                interesses_str = ""
-                for topico in interesses:
-                    self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, topico)
-                    interesses_str+=f"{topico} "
-            else:
-                interesses_str = escolhas_topico
-
-            message_regist = f"register {name} {password} {self.pull_address} {interesses_str}"
-            print(message_regist)
-            self.socket_resp.send_string(message_regist) #manda pro servidor
-            resposta = self.socket_resp.recv_string() #analisa resposta
-            print(resposta)
-            if resposta=="username inv":
-                print(f"username invalido. {name} já existe.")
-                self.register(1,interesses_str)
-            else:
-                print(f"Novo usuário registrado. Bem vindo, {name} !")
-
-
-
-    def connect_target(self, user_destino):
-        message = f"request_info {user_destino}"
-       # print(message)
-        self.socket_resp.send_string(message)
-        resposta = self.socket_resp.recv_string()
-        if resposta == "username not found" or resposta=="user offline":
-            return resposta
-        else:
-            #message = input(f"Mensagem para {user_destino}: ")
-            self.socket_push.connect(resposta)
-            return resposta
-            #self.socket_push.send_string(message)
-            #self.socket_push.disconnect(resposta)
+        self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, self.name)
 
     def run(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
         estado_enviar_msg = False
         print("Começar Loop")
         print("Para enviar uma mensagem para outro usuário digite @username_alvo")
+        print("Para ver novas mensagens digite refresh")
+        print("Para se inscrever a um tópico digite !nome_tópico")
         print("Para desconectar digite logoff")
-        fila = []
+        user_alvo = None
         while True:
-            #os.system('cls' if os.name == 'nt' else 'clear')
-            rlist, _, _ = select.select([sys.stdin,  self.socket_sub, self.socket_pull,], [], [])
-            for ready in rlist:
-                if ready == sys.stdin:
-                    if estado_enviar_msg:
-                        user_input=sys.stdin.readline()
-                        user_input = user_input.replace('\n',"")
-                        user_input = f"@{self.name}:"+ user_input
-                        self.socket_push.send_string(user_input)
-                        self.socket_push.disconnect(resposta)
-                        print(f"{Color.YELLOW} mensagem enviada {Color.RESET}")
-                        estado_enviar_msg = False
-                    else:
-                        user_input=sys.stdin.readline()
-                        user_input = user_input.replace('\n',"")
-                        if user_input=="logoff": #OPÇÃO DESLOGAR
-                            self.socket_resp.send_string(f"{user_input} {self.name}")
-                            self.socket_resp.recv_string()
-                            quit()
-                        else: #OPÇÃO MANDAR MENSAGEM PRA USUÁRIO ALVO
-                            user_alvo = user_input[1:]
-                            if user_alvo not in self.contatos:
-                                resposta = self.connect_target(user_alvo)
-                                if resposta!="username not found" and resposta!="user offline":
-                                    estado_enviar_msg = True
-                                    print(f"{Color.YELLOW} digite a mensagem para {user_alvo} {Color.RESET}")
-                                elif resposta == "username not found":
-                                    print(resposta)
-                                elif resposta == "user offline":
-                                    #Se inscreve no tópico com nome do usuário alvo para ser notificado quando o usuário ficar online e offline para atualizar a lista de contatos local
-                                    self.socket_sub.setsockopt_string(zmq.SUBSCRIBE,user_alvo)
-                                    print(f"{resposta} você será notificado quando {user_alvo} ficar online")
-                                #else:
-                                    
-
-                            user_input=""
+            if not estado_enviar_msg:
+                user_input = input("->")
+            if estado_enviar_msg:
+                user_input = input("Escreva sua mensagem:\n")
+                self.socket_push.send_string(f";;{user_alvo};;\nDe: {self.name}\n{user_input}")
+                print(f"{Color.YELLOW} mensagem enviada {Color.RESET}")
+                user_alvo = None
+                estado_enviar_msg = False
+            else:
+                if user_input=="logoff": #OPÇÃO DESLOGAR
+                    quit()
+                elif user_input=="refresh": #OPÇÃO RECEBER NOVAS MENSAGENS
+                    self.socket_push.send_string(f"refresh {self.socket_sub.getsockopt_string()}")
+                    while True:
+                        try:
+                            message_received = self.socket_sub.recv_string(flags=zmq.NOBLOCK)
+                            print(f"{Color.RED} {message_received} {Color.RESET}")
+                            print("\n")
+                        except:
+                            print("Mensagens Recebidas")
+                            break
+                elif user_input[0] == "!": #OPÇÃO SE INSCREVER A UM TÓPICO
+                    topic = user_input[1:]
+                    self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, topic)
+                    print(self.socket_sub.getsockopt_string(zmq.SUBSCRIBE))
+                elif user_input[0] == "@": #OPÇÃO MANDAR MENSAGEM PRA USUÁRIO ALVO
+                    user_alvo = user_input[1:]
+                    estado_enviar_msg = True
                 else:
-                    try: #Receber mensagem de outros usuários pelo socket_pull assincronamente
-                        message_received = self.socket_pull.recv_string(flags=zmq.NOBLOCK)
-                        fila.append(f"{Color.RED} {message_received} {Color.RESET}")
-                        print(f"{Color.RED} {message_received} {Color.RESET}")
-
-                    except :
-                        pass
-                    try: #Receber mensagem de quando um usuário de interesse ficou online/offline
-                        message_received = self.socket_sub.recv_string(flags=zmq.NOBLOCK)
-                        fila.append(f"{Color.GREEN} **server: {message_received} ** {Color.RESET}")
-                        print(F"{Color.GREEN} **server: {message_received} ** {Color.RESET}" )
-                    except:
-                        pass
-                    
-                    
-
-
-    #while True:
-       # message = socket.recv_string()
-        #topic, update = message.split()
-        #print(f"Received {topic} {update}")
+                    print("Comando não reconhecido")
 
 
 if __name__ == "__main__":
--- socket_server.py
+++ socket_server.py
@@ -4,98 +4,30 @@ import time
 
 class Server:
     def __init__(self):
+        #Sockect publisher
         self.context_pub = zmq.Context()
-        self.socket_pub = self.context_pub.socket(zmq.PUB) #cria o socket de publisher para anunciar usuários novos com interesses similares
+        self.socket_pub = self.context_pub.socket(zmq.PUB)
         self.socket_pub.bind("tcp://*:5556")
-        self.context_rep = zmq.Context()
-        self.socket_rep = self.context_rep.socket(zmq.REP) #cria o socket de resposta a requisição para registros de usuário e pedidos de dados.
-        self.socket_rep.bind("tcp://*:5557")
-        self.users_senhas = {'A':'123', 'B':'123'} #dicionário para usuário e senha
-        self.users_sockets = {} #dicionário para usuário e socket (TODO: Descobrir como fazer o usuário mandar o socket para mensagens dele.) talvez mudar nome para users_online
+
+        #Socket consumer
+        self.context_con = zmq.Context()
+        self.socket_con = self.context_con.socket(zmq.PULL)
+        self.socket_con.bind("tcp://*:5557")
+
         self.run()
-        #self.//topic = "events"
-
-    def login_users(self,parse):
-        new_message = ""
-        if parse[0] == "login": #message = "login user password pull_address"
-            user = parse[1]
-            password = parse[2]
-            pull_address = parse[3]
-            if user in self.users_senhas:
-                if self.users_senhas[user] == password:
-                    new_message = "liberado"
-                    self.users_sockets[user] = pull_address #atualiza socket do usuário
-                else:
-                    new_message = "err_senha"
-        elif parse[0] == "register": #message = register user password pull_address t0 t1 t2...
-            print("registro")
-            user = parse[1]
-            if user in self.users_senhas:
-                new_message = "username inv"
-            else:
-                password = parse[2]
-                pull_address = parse[3]
-                self.users_senhas[user] = password #registra senha
-                self.users_sockets[user] = pull_address #registra socket
-                new_message = "new_user"
-
-        return new_message
-
-    def provide_socket(self,parse):
-        _, username = parse
-        print(parse)
-        print(username)
-        if username in self.users_sockets:
-            new_message = self.users_sockets[username]
-            print(new_message)
-        else:
-            if username in self.users_senhas: #Se não está na lista de sockets (deleta o item quando o usuário desloga) mas está na lista de usuários, ele está offline
-                new_message = "user offline"
-            else:
-                new_message = "username not found"
-
-        return new_message
 
     def run(self):
         while True:
-            message = self.socket_rep.recv_string()
+            message = self.socket_con.recv_string()
             if message:
                 parse = message.split(" ")
                 op_code = parse[0]
-                if op_code == "login" or op_code == "register":
-                    new_message = self.login_users(parse)
-                    user = parse[1]
-                    pull_address = parse[3]
-                    #print(f"{user} connected {pull_address}")
-                    self.socket_pub.send_string(f"{user} connected {pull_address}")
-                elif op_code == "request_info":
-                    new_message = self.provide_socket(parse)
-                elif op_code == "logoff":
+                if op_code == "refresh":
                     user_logoff = parse[1]
                     del self.users_sockets[user_logoff]
                     new_message = "logged off"
                     print(f"{user_logoff} disconnected")
                     self.socket_pub.send_string(f"{user_logoff} disconnected")
-                
-               # print(f"respota: {new_message}")
-                self.socket_rep.send_string(new_message)
-
-
-
-                #if new_message == "new_user": #isso seria pra anunciar pro mundo que tem um novo usuário, seria absolutamente insuportável em uma aplicação de verdade com 100000 usuários que cresce todo dia
-                #    self.socket_pub.send_string(f"{new_message} {user}") #incluir depois {address} quando eu descobrir como mandar o socket de receber mensagem do usuário.
-
-            #print(self.users_senhas)
-            #print(self.users_sockets)
-
-           # message = f"Update_{request}"
-
-          #  print(f"Publishing {topic} {message}")
-
-          # socket.send_string(f"{topic} {message}")
-
-
-           # time.sleep(1)
 
 
 
